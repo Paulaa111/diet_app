34e1e2978ea7b95e60a3c9cdfb306c98328a4034
Paulaa111 <152186017+Paulaa111@users.noreply.github.com>
2026-04-21 18:47:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -312,12 +312,6 @@ with st.form("meal_form", clear_on_submit=True):
     meal_time = st.selectbox("Pora", ["Śniadanie", "II Śniadanie", "Obiad", "Kolacja", "Przekąska"])
     submitted = st.form_submit_button("DODAJ POSIŁEK", use_container_width=True)
 
-st.write("TEST:", parse_meal("2 kromki mojego chleba"))
-st.write("TEST2:", parse_meal("łyżka finuu"))
-# NOWY DEBUG
-st.write("NUTRITION:", calculate_nutrition("chleb z otrębami", 160))
-st.write("DB:", MY_FOOD_DB.get("chleb z otrębami"))
-
 if submitted and food_input:
     with st.spinner("🔍 Analizuję…"):
         items = parse_meal(food_input)
